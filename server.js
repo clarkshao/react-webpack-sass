3391a6006bf31c178279b3a17c466f42b58e8460
clarkshao <233844057@qq.com>
2016-05-03 09:35:32 +0800

    add self-defined proxy

--- server.js
+++ server.js
@@ -5,7 +5,23 @@ var config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  proxy: {
+    '/tapi/*' :  {
+      target: 'http://localhost:8888/',
+      secure :  false ,
+      bypass :  function ( req , res , proxyOptions )  {
+        console.log('daili')
+      }
+    },
+    '/get.php' :  {
+      target :  'http://guanjia.qq.com/tapi/vr/',
+      secure :  false ,
+      bypass :  function ( req , res , proxyOptions )  {
+        console.log('daili2')
+      }
+    }
+  }
 }).listen(3000, 'localhost', function (err, result) {
   if (err) {
     return console.log(err);
--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from '../component/hello';
+import $ from 'jquery';
 require('../css/base.scss');
 
 let app = document.createElement('div');
 document.body.appendChild(app);
 
 ReactDOM.render(<Hello name="NateSteven" />, app);
+
+$.getJSON('/tapi/vr/get.php',{
+    start: 0,
+    limit: 6
+},(json) => {
+    console.log(json)
+});
